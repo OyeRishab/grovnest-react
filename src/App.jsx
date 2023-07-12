bdb0ae8aab316c32d3e8f892bb9d5c4de49afb36
Rishab Jain <jainrishabbohra@gmail.com>
2023-07-12 21:55:10 +0530

    Route Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="about" element={<div>About</div>} />
+          </Route>
+          <Route path="dashboard" element={<Dashboard />}>
+            <Route path=":user" element={<div>User</div>} />
+          </Route>
+
+          <Route path="*" element={<div>Error</div>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
